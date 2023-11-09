f5b8558e00f1764bd6fd9604709559a061719ca8
Arif Nur <nnarif37@gmail.com>
2023-11-09 07:43:06 +0700

    new settings

--- src/MrMrsChefs/settings.py
+++ src/MrMrsChefs/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -48,13 +49,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
 ]
 
 ROOT_URLCONF = 'MrMrsChefs.urls'
-
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
-]
 
 TEMPLATES = [
     {
